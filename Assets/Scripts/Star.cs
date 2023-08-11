a137ff0996f9d1f23530044d35491f461a98057f
Gregory Horvath <gphorvath@gameshenanigans.com>
2023-08-10 21:55:44 -0400

    Changed starfall behavior, they disappear after falling

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -2,27 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Star : MonoBehaviour
 {
+    public float minWaitTime = 1.0f;  // Minimum time before falling
+    public float maxWaitTime = 5.0f;  // Maximum time before falling
+
+    public float bounceForce = 10.0f; // Adjust this to control bounce strength
+
+    public float minGravityScale = 0.5f; // Minimum random gravity scale
+    public float maxGravityScale = 2.0f; // Maximum random gravity scale
+
     private Rigidbody2D rb;
+    private bool isFalling = false;
+    private bool countdownStarted = false;
+    private bool hasTouchedGround = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;  // Initially, disable gravity so the object doesn't fall
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Update()
     {
-        // Check if the other collider is the target object.
-        // If you want to be more specific, you can check tags or layers, like: other.CompareTag("SomeTag")
-        if (other.gameObject.tag == "Player")
+        // Check if the object has touched the ground and stopped moving
+        if (hasTouchedGround && rb.velocity.magnitude < 0.01f)
         {
-            UnfreezeConstraints();
+            Destroy(gameObject);
         }
     }
 
-    private void UnfreezeConstraints()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        rb.constraints = RigidbodyConstraints2D.None; // This unfreezes all constraints for a 2D Rigidbody.
+        if (!countdownStarted && collision.gameObject.name == "TriggerStarfall")
+        {
+            // Set a random time before the object starts falling
+            Invoke("StartFalling", Random.Range(minWaitTime, maxWaitTime));
+            countdownStarted = true;
+        }
+    }
+
+    private void StartFalling()
+    {
+        rb.gravityScale = Random.Range(minGravityScale, maxGravityScale);  // Randomize gravity scale
+        isFalling = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isFalling)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, bounceForce);
+            isFalling = false;
+            hasTouchedGround = true;
+        }
     }
 }
