9365690cb18238e823b2f9011cacccaba41b21e7
Gregory Horvath <gphorvath@gameshenanigans.com>
2023-08-12 12:08:17 -0400

    Added second level and platform effectors

--- Assets/Scripts/FreezeStarfall.cs
+++ Assets/Scripts/FreezeStarfall.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class FreezeStarfall : MonoBehaviour
 {
     public StarfallSpawner starfallSpawner; // Reference to the StarfallSpawner script
-
     public GameObject door; // Reference to the door object
+    public bool oneTimeUse = true;
 
     private bool onCooldown = false; // Flag to check if the script is on cooldown
     private float cooldownDuration = 3f; // Duration for which the script should stay on cooldown
-
     private bool starsAreFrozen = false; // Flag to keep track of the starfall's state
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,21 +15,36 @@ public class FreezeStarfall : MonoBehaviour
         // Assuming your player has the tag "Player", adjust if necessary
         if (!onCooldown && collision.CompareTag("Player"))
         {
-            if(!starsAreFrozen)
-            {
-                door.SetActive(false);
-            }
+            Freeze();
 
-            // Toggle the state of the stars and the spawning
-            starsAreFrozen = !starsAreFrozen;
-            if (starfallSpawner)
+            DeactivateDoor();
+
+            if(oneTimeUse)
             {
-                starfallSpawner.ToggleSpawning(starsAreFrozen); 
+                this.gameObject.SetActive(false);
+                return;
             }
 
             // Activate cooldown
             onCooldown = true;
             Invoke("ResetCooldown", cooldownDuration);
+        }
+    }
+
+    private void Freeze()
+    {
+        if (starfallSpawner)
+        {
+            starfallSpawner.ToggleSpawning(!starsAreFrozen);
+            starsAreFrozen = !starsAreFrozen;
+        }
+    }
+
+    private void DeactivateDoor()
+    {
+        if(door)
+        {
+            door.SetActive(false);
         }
     }
 
